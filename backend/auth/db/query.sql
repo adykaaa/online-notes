d26ef190a53bbf77d2a3914f104aa8a1a0fb15c0
adykaaa <gosupalanta@gmail.com>
2022-11-19 09:02:12 +0100

    adding sqlc generated stuff

---
+++ backend/auth/db/query.sql
@@ -0,0 +1,20 @@
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY username;
+
+-- name: CreateUser :one
+INSERT INTO users (
+  email, username, password
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
+
