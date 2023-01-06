f96affb6b6772a585d4f2e5036532ef427e75592
adykaaa <gosupalanta@gmail.com>
2023-01-06 09:41:20 +0100

    adding user related functionality

--- backend/api/jwt.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "fmt"
-
-func main() {
-	fmt.Println("auth!")
-}
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,8 +1,57 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	ID       int    `json:"id" gorm:"primaryKey"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID       uuid.UUID `json:"id" gorm:"primaryKey"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+}
+
+func (u *User) ValidateUser() error {
+	if u.Email == "" {
+		return errors.New("User email cannot be nil!")
+	}
+
+	if u.Username == "" {
+		return errors.New("Username cannot be nil!")
+	}
+
+	if u.Password == "" {
+		return errors.New("Password cannot be nil!")
+	}
+	return nil
+}
+
+func NewUser(email string, username string, password string) (*User, error) {
+	u := &User{
+		ID:       uuid.New(),
+		Email:    email,
+		Username: username,
+		Password: password,
+	}
+	return u, nil
+}
+
+func HashUserPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", fmt.Errorf("Could not hash user password! %v", err)
+	}
+	return string(hash), nil
+}
+
+func (u *User) ValidateUserPassword(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
 }
