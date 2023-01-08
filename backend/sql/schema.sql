ada14245a162d28092b296a02c6021c849ef8a61
adykaaa <gosupalanta@gmail.com>
2023-01-08 09:15:22 +0100

    sqlc

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,5 +1,4 @@
-CREATE TABLE users 
-(
+CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(30) NOT NULL UNIQUE,
   password VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL UNIQUE,
@@ -7,11 +6,10 @@ CREATE TABLE users
   PRIMARY KEY (username)
 );
 
-CREATE TABLE notes
-(
+CREATE TABLE IF NOT EXISTS notes (
  id UUID DEFAULT gen_random_uuid (),
  title TEXT NOT NULL,
- user VARCHAR (30) references users(username),
+ user VARCHAR (30) references users(username) ON DELETE CASCADE,
  text TEXT,
  created_at TIMESTAMP,
  updated_at TIMESTAMP,
