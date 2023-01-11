c71c3c8cb2c6a74742f08b0294af481cfa05cbf3
adykaaa <gosupalanta@gmail.com>
2023-01-11 19:44:50 +0100

    adding postgresql

---
+++ backend/db/postgres.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+const (
+	defaultConnAttempts = 5
+	defaultConnTimeout  = 2 * time.Second
+)
+
+type postgres struct {
+	db           *sql.DB
+	connAttempts int
+	connTimeout  time.Duration
+}
+
+func NewPostgresDB(url string) (*postgres, error) {
+	pg := &postgres{
+		connAttempts: defaultConnAttempts,
+		connTimeout:  defaultConnTimeout,
+	}
+	var err error
+	for pg.connAttempts > 0 {
+		pg.db, err = sql.Open("postgres", string(url))
+		if err != nil {
+			fmt.Printf("Error trying to connect to the postgres DB on %s. %v.  Attempts left: %v", url, err, pg.connAttempts)
+			break
+		}
+		time.Sleep(pg.connTimeout)
+		pg.connAttempts--
+	}
+
+	return pg, nil
+}
+
+func (p *postgres) GetDB() *sql.DB {
+	return p.db
+}
+
+func (p *postgres) Close() {
+	if p.db != nil {
+		err := p.db.Close()
+		if err != nil {
+			fmt.Printf("Error closing the Postgre DB connection!")
+		}
+	}
+}
