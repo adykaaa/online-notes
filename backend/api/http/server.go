68245c05513faf4741446630904fe88e2774aa37
adykaaa <gosupalanta@gmail.com>
2023-01-11 08:57:52 +0100

    http server implemented

--- backend/api/http/server.go
+++ backend/api/http/server.go
@@ -1,1 +1,46 @@
 package http
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	server          *http.Server
+	notify          chan error
+	shutdownTimeout time.Duration
+}
+
+func NewServer(handler http.Handler, addr string) *Server {
+	s := &Server{
+		server: &http.Server{
+			Handler: handler,
+			Addr:    addr,
+		},
+		notify:          make(chan error, 1),
+		shutdownTimeout: 5 * time.Second,
+	}
+
+	s.start()
+
+	return s
+}
+
+func (s *Server) start() {
+	go func() {
+		s.notify <- s.server.ListenAndServe()
+		close(s.notify)
+	}()
+}
+
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+func (s *Server) Shutdown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return s.server.Shutdown(ctx)
+}
--- backend/go.mod
+++ backend/go.mod
@@ -3,14 +3,6 @@ module github.com/adykaaa/online-notes
 go 1.19
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.2.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
-	gorm.io/driver/postgres v1.4.6 // indirect
-	gorm.io/gorm v1.24.3 // indirect
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.5.0
 )
