b74a7a4e470ac1e06eb9f4c31daa35c097d33376
adykaaa <gosupalanta@gmail.com>
2023-01-22 15:46:35 +0100

    PASETO implementation

--- go.mod
+++ go.mod
@@ -11,10 +11,14 @@ require (
 
 require (
 	github.com/adykaaa/httplog v1.0.1-0.20230120150428-bc5215c68642 // indirect
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
+	github.com/aead/chacha20poly1305 v0.0.0-20201124145622-1a5aba2a8b29 // indirect
+	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/go-chi/render v1.0.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/o1egl/paseto v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 )
--- http/token.go
+++ http/token.go
@@ -1,28 +1,76 @@
 package http
 
-symmetricKey := []byte("YELLOW SUBMARINE, BLACK WIZARDRY") // Must be 32 bytes
-now := time.Now()
-exp := now.Add(24 * time.Hour)
-nbt := now
+import (
+	"fmt"
+	"time"
 
-jsonToken := paseto.JSONToken{
-        Audience:   "test",
-        Issuer:     "test_service",
-        Jti:        "123",
-        Subject:    "test_subject",
-        IssuedAt:   now,
-        Expiration: exp,
-        NotBefore:  nbt,
-        }
-// Add custom claim    to the token    
-jsonToken.Set("data", "this is a signed message")
-footer := "some footer"
+	"github.com/aead/chacha20poly1305"
+	"github.com/google/uuid"
+	"github.com/o1egl/paseto"
+)
 
-// Encrypt data
-token, err := paseto.Encrypt(symmetricKey, jsonToken, footer)
-// token = "v2.local.E42A2iMY9SaZVzt-WkCi45_aebky4vbSUJsfG45OcanamwXwieieMjSjUkgsyZzlbYt82miN1xD-X0zEIhLK_RhWUPLZc9nC0shmkkkHS5Exj2zTpdNWhrC5KJRyUrI0cupc5qrctuREFLAvdCgwZBjh1QSgBX74V631fzl1IErGBgnt2LV1aij5W3hw9cXv4gtm_jSwsfee9HZcCE0sgUgAvklJCDO__8v_fTY7i_Regp5ZPa7h0X0m3yf0n4OXY9PRplunUpD9uEsXJ_MTF5gSFR3qE29eCHbJtRt0FFl81x-GCsQ9H9701TzEjGehCC6Bhw.c29tZSBmb290ZXI"
+type PasetoPayload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issuedAt"`
+	ExpiresAt time.Time `json:"expiresAt"`
+}
 
-// Decrypt data
-var newJsonToken paseto.JSONToken
-var newFooter string
-err := paseto.Decrypt(token, symmetricKey, &newJsonToken, &newFooter)
+func NewPasetoPayload(username string) (*PasetoPayload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Errorf("Could not generate a random token ID! %v", err)
+		return nil, err
+	}
+
+	payload := &PasetoPayload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(5 * time.Minute),
+	}
+
+	return payload, nil
+}
+
+type Paseto struct {
+	key    []byte
+	paseto *paseto.V2
+}
+
+func NewPaseto(symmetricKey string) (*Paseto, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size, must be %d bytes", chacha20poly1305.KeySize)
+	}
+
+	paseto := &Paseto{
+		key:    []byte(symmetricKey),
+		paseto: paseto.NewV2(),
+	}
+	return paseto, nil
+}
+
+func (maker *Paseto) CreateToken(username string) (string, *PasetoPayload, error) {
+	payload, err := NewPasetoPayload(username)
+	if err != nil {
+		return "", payload, err
+	}
+
+	token, err := maker.paseto.Encrypt(maker.key, payload, nil)
+	return token, payload, err
+}
+
+func (maker *Paseto) VerifyToken(token string) (*PasetoPayload, error) {
+	payload := &PasetoPayload{}
+
+	err := maker.paseto.Decrypt(token, maker.key, payload, nil)
+	if err != nil {
+		return nil, fmt.Errorf("the PASETO is invalid!")
+	}
+
+	if time.Now().After(payload.ExpiresAt) {
+		return nil, fmt.Errorf("the PASETO has expired")
+	}
+
+	return payload, nil
+}
