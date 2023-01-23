f6ae94a0292c65f8b991499bae887e0a0f3e8290
adykaaa <gosupalanta@gmail.com>
2023-01-23 12:02:24 +0100

    added unittest file

--- utils/password_test.go
+++ utils/password_test.go
@@ -2,28 +2,8 @@ package utils
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestHashUserPassword(t *testing.T) {
-	password := "abc123"
-	hashedPassword, err := HashUserPassword(password)
 
-	require.NoError(t, err)
-	require.NotEmpty(t, hashedPassword)
-}
-
-func TestValidateUserPassword(t *testing.T) {
-	tc := []struct{
-		name string
-		password string
-		hashedPassword string
-	}{
-		{
-			name: "Validation is OK",
-			password: "abc123",
-			hashedPassword: HashUserPassword(password),
-		}
-	}
 }
