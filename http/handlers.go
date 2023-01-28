ba6b2f0729ce8d55fdaf585ee0a9d9ef561e2964
adykaaa <gosupalanta@gmail.com>
2023-01-28 18:23:38 +0100

    handler mod

--- http/handlers.go
+++ http/handlers.go
@@ -1,12 +1,15 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	sqlc "github.com/adykaaa/online-notes/db/sqlc"
 	models "github.com/adykaaa/online-notes/http/models"
 	"github.com/adykaaa/online-notes/utils"
+	"github.com/go-chi/render"
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog"
 )
@@ -20,37 +23,49 @@ func Home(q sqlc.Querier) http.HandlerFunc {
 func RegisterUser(q sqlc.Querier) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var user models.User
-		l := zerolog.Ctx(r.Context())
+
+		w.Header().Set("Content-Type", "application/json")
+		ctx, cancel := context.WithTimeout(r.Context(), 500*time.Millisecond)
+		defer cancel()
+
+		l := zerolog.Ctx(ctx)
 
 		err := json.NewDecoder(r.Body).Decode(&user)
 		if err != nil {
 			l.Error().Err(err).Msgf("error decoding the User into JSON during registration. %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			render.JSON(w, r, "internal server error during JSON decoding")
 			return
 		}
 
 		validate := validator.New()
 		err = validate.Struct(&user)
 		if err != nil {
-			http.Error(w, "failed to validate submitted user creds!", 400)
+			l.Error().Err(err).Msgf("error during User struct validation %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			render.JSON(w, r, "bad or missing user parameter was provided")
 			return
 		}
 
 		hashedPassword, err := utils.HashPassword(user.Password)
 		if err != nil {
 			l.Error().Err(err).Msgf("error during password hashing %v", err)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 
-		uname, err := q.RegisterUser(r.Context(), &sqlc.RegisterUserParams{
+		uname, err := q.RegisterUser(ctx, &sqlc.RegisterUserParams{
 			Username: user.Username,
 			Password: hashedPassword,
 			Email:    user.Email,
 		})
 		if err != nil {
 			l.Error().Err(err).Msgf("Error during user registration to the DB! %v", err)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 
+		render.JSON(w, r, "User registration successful!")
 		l.Info().Msgf("User registration for %s was successful!", uname)
 	}
 }
