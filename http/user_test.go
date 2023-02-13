6e7850b30d79a8dbd99ea57bb2f0e3903b3e2b6c
adykaaa <gosupalanta@gmail.com>
2023-02-13 17:24:39 +0100

    working on user unit tests

--- http/user_test.go
+++ http/user_test.go
@@ -6,19 +6,60 @@ import (
 	"testing"
 
 	mockdb "github.com/adykaaa/online-notes/db/mock"
+	db "github.com/adykaaa/online-notes/db/sqlc"
 	models "github.com/adykaaa/online-notes/http/models"
+	"github.com/go-playground/validator/v10"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRegisterUser(t *testing.T) {
 	ctrl := gomock.NewController(t)
-	mockdb := mockdb.NewMockQuerier(ctrl)
+	dbmock := mockdb.NewMockQuerier(ctrl)
+	jsonValidator := validator.New()
 	ctx := context.Background()
 
-	tc := []struct {
+	testCases := []struct {
 		name          string
-		body          models.User
-		dbmock        func(db *mockdb.MockQuerier)
+		body          *models.User
+		validateJSON  func(v *validator.Validate, body *models.User)
+		dbmock        func(mockdb *mockdb.MockQuerier, body *models.User)
 		checkResponse func(recoder *httptest.ResponseRecorder)
-	}{}
+	}{
+		{
+			name: "User registration OK",
+
+			body: &models.User{
+				Username: "user1",
+				Password: "password1",
+				Email:    "user1@user.com",
+			},
+
+			validateJSON: func(v *validator.Validate, body *models.User) {
+				err := v.Struct(body)
+				require.NoError(t, err)
+			},
+
+			dbmock: func(mockdb *mockdb.MockQuerier, body *models.User) {
+				args := db.RegisterUserParams{
+					Username: body.Username,
+					Password: body.Password,
+					Email:    body.Email,
+				}
+
+				mockdb.EXPECT().RegisterUser(ctx, &args).Return(args.Username, nil)
+				user, err := mockdb.RegisterUser(ctx, &args)
+				require.NoError(t, err)
+				require.Equal(t, user, args.Username)
+			},
+			checkResponse: func(recoder *httptest.ResponseRecorder) {
+			},
+		},
+	}
+	for c := range testCases {
+
+		testCases[c].validateJSON(jsonValidator, testCases[c].body)
+
+	}
+
 }
