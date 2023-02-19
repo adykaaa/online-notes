6d238cab7db60eefd647591d60d14e6f298cd810
adykaaa <gosupalanta@gmail.com>
2023-02-19 12:25:28 +0100

    started wroking on auth test

--- http/auth_test.go
+++ http/auth_test.go
@@ -1,1 +1,36 @@
 package http
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	models "github.com/adykaaa/online-notes/http/models"
+)
+
+func MockHandler(w http.ResponseWriter, r *http.Request) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Test server started!")
+	}))
+	defer svr.Close()
+
+}
+
+func TestAuthMiddleware(t *testing.T) {
+	testCases := []struct {
+		name          string
+		body          *models.User
+		handler       func(t *testing.T, w http.ResponseWriter, r *http.Request)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder, request *http.Request)
+	}{}
+
+	for c := range testCases {
+		tc := testCases[c]
+
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, "/notes/create", nil)
+			w := httptest.NewRecorder()
+		})
+	}
+}
